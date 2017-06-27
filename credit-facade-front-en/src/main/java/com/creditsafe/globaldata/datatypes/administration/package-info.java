9d0b54faeae248744efb0baddcb9f26ca85407e8
mikegu007 <273210563@qq.com>
2017-06-27 15:07:02 +0800

    front /cnen

--- credit-facade-front-en/src/main/java/com/creditsafe/globaldata/datatypes/administration/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.creditsafe.com/globaldata/datatypes/administration", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.creditsafe.globaldata.datatypes.administration;
--- credit-facade-front-en/src/main/java/com/creditsafe/globaldata/operations/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.creditsafe.com/globaldata/operations", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.creditsafe.globaldata.operations;
